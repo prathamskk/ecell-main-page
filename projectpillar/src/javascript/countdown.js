0d1080cf5957a903a8d2f1070387d8e754c8c91d
prathamskk <prathamskk@gmail.com>
2022-07-11 11:06:55 +0530

    project page updates

--- projectpillar/src/javascript/countdown.js
+++ projectpillar/src/javascript/countdown.js
@@ -1,27 +1,82 @@
-const countToDate = new Date().setHours(
-  new Date(11 - 06 - 2022).getHours() + 24
-);
-let previousTimeBetweenDates;
+const flipCard = document.querySelector(".flip-card");
+const time = { hour: 12, minute: 00 };
+const countToDate = new Date("Jul 11, 2022 14:00:00").getTime();
+
 setInterval(() => {
-  const currentDate = new Date();
-  const timeBetweenDates = Math.ceil((countToDate - currentDate) / 1000);
-  flipAllCards(timeBetweenDates);
+  const currentDate = new Date().getTime();
+  const timeLeft = countToDate - currentDate;
 
-  previousTimeBetweenDates = timeBetweenDates;
-}, 250);
+  const daysSetter = 1000 * 60 * 60 * 24;
+  const hoursSetter = 1000 * 60 * 60;
+  const minutesSetter = 1000 * 60;
+  const secondsSetter = 1000;
 
-function flipAllCards(time) {
-  const seconds = time % 60;
-  const minutes = Math.floor(time / 60) % 60;
-  const hours = Math.floor(time / 3600);
+  const days = Math.floor(countToDate / daysSetter - currentDate / daysSetter);
+  const hours = Math.floor(
+    (countToDate / hoursSetter - currentDate / hoursSetter) % 24
+  );
+  const minutes = Math.floor(
+    (countToDate / minutesSetter - currentDate / minutesSetter) % 60
+  );
+  const seconds = Math.floor(
+    (countToDate / secondsSetter - currentDate / secondsSetter) % 60
+  );
 
+  // console.log(
+  //   "days: " +
+  //     days +
+  //     " hours: " +
+  //     hours +
+  //     " mins: " +
+  //     minutes +
+  //     " seconds: " +
+  //     seconds
+  // );
+
+  flip(document.querySelector("[data-days-tens]"), Math.floor(days / 10));
+  flip(document.querySelector("[data-days-ones]"), days % 10);
   flip(document.querySelector("[data-hours-tens]"), Math.floor(hours / 10));
   flip(document.querySelector("[data-hours-ones]"), hours % 10);
   flip(document.querySelector("[data-minutes-tens]"), Math.floor(minutes / 10));
   flip(document.querySelector("[data-minutes-ones]"), minutes % 10);
   flip(document.querySelector("[data-seconds-tens]"), Math.floor(seconds / 10));
   flip(document.querySelector("[data-seconds-ones]"), seconds % 10);
-}
+}, 250);
+
+// function flip(flipCard, passedNumber) {
+//   const topHalf = document.querySelector(".top");
+//   const startNumber = parseInt(topHalf.textContent);
+
+//   if (passedNumber === startNumber) return;
+
+//   const bottomHalf = document.querySelector(".bottom");
+
+//   const topFlip = document.createElement("div");
+//   topFlip.classList.add("top-flip");
+
+//   const bottomFlip = document.createElement("div");
+//   bottomFlip.classList.add("bottom-flip");
+
+//   topHalf.textContent = startNumber;
+//   bottomHalf.textContent = startNumber;
+
+//   topFlip.textContent = startNumber;
+//   bottomFlip.textContent = passedNumber;
+
+//   topFlip.addEventListener("animationstart", (e) => {
+//     topHalf.textContent = passedNumber;
+//   });
+//   topFlip.addEventListener("animationend", (e) => {
+//     topFlip.remove();
+//   });
+
+//   bottomFlip.addEventListener("animationend", (e) => {
+//     bottomHalf.textContent = passedNumber;
+//     bottomFlip.remove();
+//   });
+
+//   flipCard.append(topFlip, bottomFlip);
+// }
 
 function flip(flipCard, newNumber) {
   const topHalf = flipCard.querySelector(".top");
