07eaddca83d62c1c2a80f0349d4e43c9805491c1
prathamskk <prathamskk@gmail.com>
2022-07-12 09:53:21 +0530

    SAKEC logo added to common img

--- projectpillar/src/javascript/countdown.js
+++ projectpillar/src/javascript/countdown.js
@@ -1,6 +1,6 @@
 const flipCard = document.querySelector(".flip-card");
 const time = { hour: 12, minute: 00 };
-const countToDate = new Date("Jul 11, 2022 14:00:00").getTime();
+const countToDate = new Date("Aug 10, 2022 12:00:00").getTime();
 
 setInterval(() => {
   const currentDate = new Date().getTime();
