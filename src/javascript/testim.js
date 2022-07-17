8e747b551a0aa6507769b1879d73acee77bc1670
Varun Rajapure <78044274+CodeCheck1107@users.noreply.github.com>
2022-07-17 17:55:24 +0530

    change fix on testimony

--- src/javascript/testim.js
+++ src/javascript/testim.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Slider
 const slider = function () {
     const slides = document.querySelectorAll('.slide');
@@ -83,46 +82,3 @@ const slider = function () {
     });
   };
   slider();
-=======
-var galleryThumbs = new Swiper('.gallery-thumbs', {
-	effect: 'coverflow',
-	grabCursor: true,
-	centeredSlides: true,
-	slidesPerView: '2',
-	// coverflowEffect: {
-	//   rotate: 50,
-	//   stretch: 0,
-	//   depth: 100,
-	//   modifier: 1,
-	//   slideShadows : true,
-	// },
-	
-	coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 50,
-        modifier: 6,
-        slideShadows : false,
-	  },
-	  
-  });
-  
-  
-var galleryTop = new Swiper('.swiper-container.testimonial', {
-	speed: 400,
-	spaceBetween: 50,
-	autoplay: {
-	  delay: 3000,
-	  disableOnInteraction: false,
-	},
-	direction: 'vertical',
-	pagination: {
-	  clickable: true,
-	  el: '.swiper-pagination',
-	  type: 'bullets',
-	},
-	thumbs: {
-		swiper: galleryThumbs
-	  }
-  });
->>>>>>> fa0ab888c82844144e28dfba57418629d35735dc
